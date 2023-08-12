49648dc6fc4ab706360b4d310258f2b3eb8ec2d7
Adnan Sait <adnansait74@gmail.com>
2023-08-12 18:49:36 +0530

    Feat: Code Coverage for component tests
    Added code coverage configuration for component tests

--- cypress.config.js
+++ cypress.config.js
@@ -17,6 +17,41 @@ module.exports = defineConfig({
     devServer: {
       framework: "create-react-app",
       bundler: "webpack",
+      /**
+       * @see {@link https://glebbahmutov.com/blog/component-code-coverage/}
+       */
+      webpackConfig: {
+        mode: "development",
+        devtool: false,
+        module: {
+          rules: [
+            // application and Cypress files are bundled like React components
+            // and instrumented using the babel-plugin-istanbul
+            // (we will filter the code coverage for non-application files later)
+            {
+              test: /(\.js|\.jsx)$/,
+              exclude: /node_modules/,
+              use: {
+                loader: "babel-loader",
+                options: {
+                  presets: ["@babel/preset-env", "@babel/preset-react"],
+                  plugins: [
+                    // we could optionally insert this plugin
+                    // only if the code coverage flag is on
+                    "istanbul",
+                  ],
+                },
+              },
+            },
+          ],
+        },
+      },
+    },
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+      require("@cypress/code-coverage/task")(on, config);
+
+      return config;
     },
     video: false,
     screenshotOnRunFailure: false,
